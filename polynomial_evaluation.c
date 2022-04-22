f13bed2f2164acf9734321d9c43b25ab50e78efb
FUCK1YOU2HOLE <100886327+FUCK1YOU2HOLE@users.noreply.github.com>
2022-04-22 09:27:12 +0800

    取個位數_多項式賦值

---
+++ polynomial_evaluation.c
@@ -0,0 +1,16 @@
+# tyuy
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double x,sum;
+    int y;
+    scanf("%lf%d",&x,&y);
+    double A[y];
+    int B[y];
+    for(int i=0 ;i<y;i++){
+        scanf("%lf%d",&A[i],&B[i]);
+        sum+=A[i]*pow(x,B[i]);
+    }
+    printf("%lf",sum);
+}
